f5cb16ca08dac421c0f90ce2f5451081a6d86b8a
Frizlab <fload@me.com>
2018-05-31 00:10:21 +0200

    Finish implementation of the program

---
+++ find_unreferenced_xcode_files/Tree.swift
@@ -0,0 +1,71 @@
+/*
+ * Tree.swift
+ * find_unreferenced_xcode_files
+ *
+ * Created by François Lamboley on 30/05/2018.
+ * Copyright © 2018 Frizlab. All rights reserved.
+ */
+
+import Foundation
+
+
+
+class XcodeNode {
+	
+	class func from(reference: String, in pbxprojObjects: [String: Any]) -> XcodeNode? {
+		guard let object = pbxprojObjects[reference] as? [String: Any] else {return nil}
+		
+		switch object["isa"] as? String {
+		case "PBXGroup":         return Group(pbxprojObject: object, pbxprojObjects: pbxprojObjects)
+		case "PBXFileReference": return FileReference(pbxprojObject: object, pbxprojObjects: pbxprojObjects)
+		default:                 return nil
+		}
+	}
+	
+	func getFilePaths(prefix: String) -> [String] {
+		fatalError("abstract method")
+	}
+	
+}
+
+class Group : XcodeNode {
+	
+	private(set) var relativePath: String?
+	private(set) var children = [XcodeNode]()
+	
+	init?(pbxprojObject object: [String: Any], pbxprojObjects: [String: Any]) {
+		guard object["isa"] as? String == "PBXGroup" else {return nil}
+		
+		guard let p = object["path"] as? String? else {return nil}
+		guard let c = object["children"] as? [String] else {return nil}
+		
+		relativePath = p
+		children = c.compactMap{ XcodeNode.from(reference: $0, in: pbxprojObjects) }
+		
+		super.init()
+	}
+	
+	override func getFilePaths(prefix: String) -> [String] {
+		return children.flatMap{ $0.getFilePaths(prefix: (prefix as NSString).appendingPathComponent(relativePath ?? "")) }
+	}
+	
+}
+
+class FileReference : XcodeNode {
+	
+	private(set) var relativePath: String
+	
+	init?(pbxprojObject object: [String: Any], pbxprojObjects: [String: Any]) {
+		guard object["isa"] as? String == "PBXFileReference" else {return nil}
+		
+		guard let p = object["path"] as? String else {return nil}
+		relativePath = p
+
+		super.init()
+	}
+	
+	override func getFilePaths(prefix: String) -> [String] {
+		return [(prefix as NSString).appendingPathComponent(relativePath)]
+	}
+	
+}
--- find_unreferenced_xcode_files/main.swift
+++ find_unreferenced_xcode_files/main.swift
@@ -25,20 +25,47 @@ struct CLIError : Error {
 	
 	let code: Int32
 	let message: String?
+	let showUsage: Bool
 	
 }
 
-var expectsNull = false
-let pbxprojPath: String
 do {
+	var expectsNull = false
+	let pbxprojPath: String
 	switch CommandLine.arguments.count {
 	case 2: pbxprojPath = CommandLine.arguments[1]
-	case 3: pbxprojPath = CommandLine.arguments[2]; expectsNull = true; guard CommandLine.arguments[1] == "-0" else {throw CLIError(code: 1, message: "syntax error")}
-	default: throw CLIError(code: 1, message: "syntax error")
+	case 3: pbxprojPath = CommandLine.arguments[2]; expectsNull = true; guard CommandLine.arguments[1] == "-0" else {throw CLIError(code: 1, message: "syntax error", showUsage: true)}
+	default: throw CLIError(code: 1, message: "syntax error", showUsage: true)
 	}
-} catch  {
-	usage(progname: CommandLine.arguments[0], stream: &stderrStream)
-	exit((error as? CLIError)?.code ?? 255)
+	
+	let pbxprojData = try Data(contentsOf: URL(fileURLWithPath: pbxprojPath, isDirectory: false))
+	let pbxproj = try PropertyListSerialization.propertyList(from: pbxprojData, options: [], format: nil) as? [String: Any]
+	guard
+		let pbxprojRoot = pbxproj?["rootObject"] as? String,
+		let pbxprojObjects = pbxproj?["objects"] as? [String: Any],
+		let mainGroupId = (pbxprojObjects[pbxprojRoot] as? [String: Any])?["mainGroup"] as? String,
+		let mainGroup = pbxprojObjects[mainGroupId] as? [String: Any],
+		let filepathsArray = Group(pbxprojObject: mainGroup, pbxprojObjects: pbxprojObjects)?.getFilePaths(prefix: ".")
+	else {
+		throw NSError(domain: "", code: 1, userInfo: [NSLocalizedDescriptionKey: "Unexpected file format"])
+	}
+	
+	let filepathsSet = Set(filepathsArray)
+	
+	/* I hate this (absolute path to /dev/stdin), but SimpleStream does not support FileHandle (yet) */
+	let inputStream = InputStream(fileAtPath: "/dev/stdin")!
+	inputStream.open(); defer {inputStream.close()}
+	let simpleStream = SimpleInputStream(stream: inputStream, bufferSize: 1024, streamReadSizeLimit: nil)
+	while let e = try? simpleStream.readData(upToDelimiters: [expectsNull ? Data([0]) : Data("\n".utf8)], matchingMode: .anyMatchWins, includeDelimiter: false, alwaysCopyBytes: false) {
+		_ = try? simpleStream.readData(size: 1, alwaysCopyBytes: false) /* Read the delimiter */
+		guard let p = String(data: e, encoding: .utf8) else {continue}
+		if !filepathsSet.contains(p) {print(p)}
+	}
+} catch let error as CLIError {
+	if let msg = error.message {print("error: \(msg)", to: &stderrStream)}
+	if error.showUsage {usage(progname: CommandLine.arguments[0], stream: &stderrStream)}
+	exit(error.code)
+} catch {
+	print("unknown error: \(error.localizedDescription)", to: &stderrStream)
+	exit(255)
 }
-
-print(pbxprojPath)
